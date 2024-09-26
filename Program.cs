74eed3e53d6f2e92d4ef10f55cc4d7c3108ce28e
Bradey <bstephens@catalisgov.com>
2024-09-25 19:43:35 -0700

    simplify

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
+using BareNakedBlazor;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
-using BareNakedBlazor;
 
 var builder = WebApplication.CreateBuilder();
 builder.Services.AddRazorComponents();
@@ -8,10 +8,8 @@ builder.Services.AddRazorComponents();
 var app = builder.Build();
 
 app.MapGet("/", () => Results.Redirect("/hello"));
-
 app.MapGet("/hello", () => new RazorComponentResult<Hello>());
-
-app.MapPost("/hello", ([FromForm] string firstName) => 
+app.MapPost("/hello", ([FromForm] string firstName) =>
    new RazorComponentResult<Hello>(new { FirstName = firstName })
 ).DisableAntiforgery();
 
